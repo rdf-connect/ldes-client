50733e81030f4bfc19be52f6f897f302413fb833
Pieter Colpaert <pieter.colpaert@ugent.be>
2023-09-28 14:34:51 +0200

    Added bins and test README

---
+++ bin/validate.ts
@@ -0,0 +1,8 @@
+// Validates an LDES
+
+import { CBDShapeExtractor } from "extract-cbd-shape";
+
+// Step 1: check whether all necessary components are available to be a valid LDES.
+
+
+let extractor = new CBDShapeExtractor();
