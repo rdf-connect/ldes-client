2401be7dbb56317924e8e7f8876bcc59566fcb1c
github-action-benchmark <github@users.noreply.github.com>
2024-03-02 20:58:24 +0000

    add Benchmark for ldes client (benchmarkjs) benchmark result for e940a4d2791fe90119758152680a52ed57b64466

---
+++ docs/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1709413104099,
+  "repoUrl": "https://github.com/TREEcg/ldes-client",
+  "entries": {
+    "Benchmark for ldes client": [
+      {
+        "commit": {
+          "author": {
+            "email": "xueying_deng@outlook.com",
+            "name": "XD",
+            "username": "xdxxxdx"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e940a4d2791fe90119758152680a52ed57b64466",
+          "message": "Merge pull request #16 from xdxxxdx/feat/perf\n\nFeat/perf",
+          "timestamp": "2024-03-02T21:57:54+01:00",
+          "tree_id": "adea1ba766488be91869b5470d3cedc3f7fbe8b6",
+          "url": "https://github.com/TREEcg/ldes-client/commit/e940a4d2791fe90119758152680a52ed57b64466"
+        },
+        "date": 1709413103669,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "descending tree, emits ordered",
+            "value": 574037,
+            "range": "±15.10%",
+            "unit": "ops/sec",
+            "extra": "70 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
