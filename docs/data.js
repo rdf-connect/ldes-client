7992ed3a3f03473fe5cbb33353c05b66faf204f3
github-action-benchmark <github@users.noreply.github.com>
2024-03-06 12:55:39 +0000

    add Benchmark for ldes client (benchmarkjs) benchmark result for d803e22cc949f070a89a34aa282447c73373ed46

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1709413104099,
+  "lastUpdate": 1709729739351,
   "repoUrl": "https://github.com/TREEcg/ldes-client",
   "entries": {
     "Benchmark for ldes client": [
@@ -31,6 +31,37 @@ window.BENCHMARK_DATA = {
             "range": "±15.10%",
             "unit": "ops/sec",
             "extra": "70 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "xueying_deng@outlook.com",
+            "name": "XD",
+            "username": "xdxxxdx"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d803e22cc949f070a89a34aa282447c73373ed46",
+          "message": "Update perftest.js",
+          "timestamp": "2024-03-06T13:55:06+01:00",
+          "tree_id": "2f6e3197a31acc8a0d8db5e538c9cc0f059c47b9",
+          "url": "https://github.com/TREEcg/ldes-client/commit/d803e22cc949f070a89a34aa282447c73373ed46"
+        },
+        "date": 1709729738273,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "descending tree, emits ordered",
+            "value": 520749,
+            "range": "±20.23%",
+            "unit": "ops/sec",
+            "extra": "69 samples"
           }
         ]
       }
