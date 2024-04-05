d3a131f02c3f38d0d98f1c459fe1e5f94bc57097
Julian Rojas <julian.rojas87@gmail.com>
2024-04-05 16:12:50 +0200

    Remove dist folder

--- dist/lib/storage.d.ts
+++
@@ -1,7 +0,0 @@
-interface Storage {
-    getItem(key: string): string;
-    setItem(key: string, value: string): void;
-    removeItem(key: string): void;
-}
-declare let storage: Storage;
-export { storage };
