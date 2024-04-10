cb2e6dbec6a875bf58e71ea1b105fe25d1a59b52
github-action-benchmark <github@users.noreply.github.com>
2024-04-10 13:16:05 +0000

    add Benchmark for ldes client (benchmarkjs) benchmark result for fba3df7486f229c131260ad29fefc8d5eb26d5f0

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712577218970,
+  "lastUpdate": 1712754965042,
   "repoUrl": "https://github.com/rdf-connect/ldes-client",
   "entries": {
     "Benchmark for ldes client": [
@@ -124,6 +124,37 @@ window.BENCHMARK_DATA = {
             "range": "±6.61%",
             "unit": "ops/sec",
             "extra": "76 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "arthur.vercruysse@outlook.com",
+            "name": "ajuvercr",
+            "username": "ajuvercr"
+          },
+          "committer": {
+            "email": "arthur.vercruysse@outlook.com",
+            "name": "ajuvercr",
+            "username": "ajuvercr"
+          },
+          "distinct": true,
+          "id": "fba3df7486f229c131260ad29fefc8d5eb26d5f0",
+          "message": "also compile tests, but don't publish them",
+          "timestamp": "2024-04-10T15:15:30+02:00",
+          "tree_id": "764d6576565e9e10d1a2f22760deeef186af5e35",
+          "url": "https://github.com/rdf-connect/ldes-client/commit/fba3df7486f229c131260ad29fefc8d5eb26d5f0"
+        },
+        "date": 1712754964444,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "tree-3-3",
+            "value": 1.5,
+            "range": "±1.44%",
+            "unit": "ops/sec",
+            "extra": "12 samples"
           }
         ]
       }
