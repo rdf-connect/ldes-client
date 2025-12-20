09f148b85a80bbd2b9a8a024c277ac2b7714d956
julianrojas87 <julian.rojas87@gmail.com>
2025-12-20 17:11:57 +0100

    Remove unused state types

--- lib/state/state.ts
+++ lib/state/state.ts
@@ -1,50 +1,5 @@
 import { storage } from "./storage";
 
-export interface State {
-    init(): Promise<void>;
-
-    seen(id: string): boolean;
-
-    filter<T>(ids: T[], getId: (item: T) => string): T[];
-
-    add(id: string): void;
-
-    save(): Promise<void>;
-}
-
-export class SimpleState implements State {
-    state: Set<string>;
-    location: string;
-
-    constructor(location: string) {
-        this.location = location;
-        this.state = new Set();
-    }
-
-    async init() {
-        // Loaad location into state, or default
-        // Take into account nodejs and browser runtimes
-        //
-        // Setup on exit hooks
-    }
-
-    filter<T>(ids: T[], getId: (item: T) => string): T[] {
-        return ids.filter(async (x) => !this.seen(getId(x)));
-    }
-
-    seen(id: string): boolean {
-        return this.state.has(id);
-    }
-
-    add(id: string): void {
-        this.state.add(id);
-    }
-
-    async save(): Promise<void> {
-        // Save state into location
-    }
-}
-
 export type FileStateFactoryItem<T> = {
     name: string;
     state: StateT<T>;
